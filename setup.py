7f9e32a426f96b3e356109ba212e7c1c1e509b15
Ladislao <vladX09rus@gmail.com>
2017-12-24 21:07:15 +0300

    Fix Pillow dependency name in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         "fuzzywuzzy==0.15.1",
         "numpy==1.13.1",
         "opencv.python==3.3.0.10",
-        "Pilow==4.2.1",
+        "Pillow==4.2.1",
         "requests==2.18.4",
         "tesserocr==2.2.2"
     ],
